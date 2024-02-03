76782d61fdc1f70563f6d5cdb4f0937faf376fba
Octopus <klinov.artem@gmail.com>
2024-02-03 22:53:35 +0500

    bot_movie com12

--- loader.py
+++ loader.py
@@ -1,10 +1,10 @@
-
+"""Модуль для подгрузки переменных."""
 
 
 from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
 
-
+# глобальая переменная для хранения:
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
