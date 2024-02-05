16a20fb50ce1865cd56a512a6abc8af8b0a8aacb
Octopus <klinov.artem@gmail.com>
2024-02-05 16:21:55 +0500

    bot_movie com21 pop

--- api/popular_today.py
+++ api/popular_today.py
@@ -1,30 +1,26 @@
 """Модуль для работы с API.
 
-Получает самый популярный фильм на сегодня.
+Получает популярный фильм на сегодня из новинок.
 """
 
 
 import requests
 
+from config_data import config
 from io import BytesIO
 
 
 def popular_today():
-    url = 'https://api.kinopoisk.dev/v1.4/list/movies/popular-films'
+    url = (
+        f'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=1&'
+        f'selectFields=name&sortField=rating.kp&sortType=-1&year=2024&lists=popular-films'
+    )
 
-{
-  "category": "Фильмы",
-  "name": "Популярные фильмы",
-  "slug": "popular-films",
-  "moviesCount": 1000,
-  "cover": {
-    "url": "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/5e785e92-af07-485c-b4b2-89b64b32c6d9/orig",
-    "previewUrl": "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/5e785e92-af07-485c-b4b2-89b64b32c6d9/x1000"
-  },
-  "createdAt": "2023-09-23T20:05:14.042Z",
-  "updatedAt": "2023-09-23T20:05:14.042Z"
-}
+    if url is not None:
 
+        headers = {'accept': 'application/json', "X-API-KEY": config.API_KEY}
+        response = requests.get(url, headers=headers)
 
-
-    ...
+        if response.status_code == 200:
+            data = response.json()
+            contents = data.get('docs')
