0c58cdee0c214a7ac3e4f8d52a37ca9a7e1d14dc
Octopus <klinov.artem@gmail.com>
2024-02-05 16:41:47 +0500

    bot_movie com22 pop

--- api/popular_today.py
+++ api/popular_today.py
@@ -12,8 +12,11 @@ from io import BytesIO
 
 def popular_today():
     url = (
-        f'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=1&'
-        f'selectFields=name&sortField=rating.kp&sortType=-1&year=2024&lists=popular-films'
+        f'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&'
+        f'selectFields=name&selectFields=rating&selectFields=movieLength&'
+        f'selectFields=genres&selectFields=countries&selectFields=poster&'
+        f'selectFields=premiere&sortField=rating.kp&sortType=-1&'
+        f'year=2024&lists=popular-films'
     )
 
     if url is not None:
@@ -24,3 +27,12 @@ def popular_today():
         if response.status_code == 200:
             data = response.json()
             contents = data.get('docs')
+
+            for content in contents:
+                poster = content.get('poster', {}).get('previewUrl')
+                name = content.get('name')
+                rating = content.get('rating', {}).get('kp')
+                movieLength = content.get('movieLength')
+
+                genres_data = content.get('genres', [])
+                genres = ', '.join(genre.get('name') for genre in genres_data)
