44efbae91d006339de974263c82666a069db7159
Octopus <klinov.artem@gmail.com>
2024-02-07 13:59:01 +0500

    bot_movie com26 keyboard

--- keyboards/inline/main_keyboard.py
+++ keyboards/inline/main_keyboard.py
@@ -13,7 +13,9 @@ def main_keyboard():
     """
     keyboard = InlineKeyboardMarkup()
     button_1 = InlineKeyboardButton(text='Популярное', callback_data='pop')
+    button_2 = InlineKeyboardButton(text='Ожидаемое', callback_data='await')
+    button_3 = InlineKeyboardButton(text='Новинки', callback_data='new')
 
-    keyboard.add(button_1)
+    keyboard.add(button_1, button_2, button_3)
 
     return keyboard
