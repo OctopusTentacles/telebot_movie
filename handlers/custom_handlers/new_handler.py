8676853aa119c0c22a00d8b675dc752dc3f3820c
Octopus <klinov.artem@gmail.com>
2024-02-18 22:10:19 +0600

    bot_movie com58 new handler films

---
+++ handlers/custom_handlers/new_handler.py
@@ -0,0 +1,25 @@
+"""Модуль команды new из кнопки ГЛАВНОГО МЕНЮ Новинки."""
+
+
+from api import new_films_api
+
+from keyboards.inline import type_keyboard
+from loader import bot
+from telebot.types import CallbackQuery
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'new')
+def bot_new_content(call: CallbackQuery):
+    bot.send_message(
+        call.message.chat.id, 'НОВИНКИ: ВЫБЕРИ ТИП:',
+        reply_markup=type_keyboard()
+    )
+
+@bot.callback_query_handler(func=lambda call: call.data == 'film')
+def callback_new_film(call: CallbackQuery):
+    film_info = new_films_api.new_films_api()
+    bot.send_message(call.message.chat.id, film_info)
+    bot.send_message(
+        call.message.chat.id, 'НОВИНКИ: ВЫБЕРИ ТИП: или /main',
+        reply_markup=type_keyboard
+    )
