41b2c28c99dab4fdfd9e6701b3e1fabb792eb33f
Octopus <klinov.artem@gmail.com>
2024-02-19 20:09:19 +0600

    bot_movie com61 UserInputState

--- states/user_input.py
+++ states/user_input.py
@@ -1,1 +1,18 @@
+"""Модуль состояний пользователя."""
+
+
 from telebot.handler_backends import State, StatesGroup
+
+
+class UserInputState(StatesGroup):
+    main = State()
+    history = State()
+    help = State()
+
+    pop = State()
+    wait = State()
+    new = State()
+
+    film = State()
+    serial = State()
+    cartoon = State()
