466c03257d1de53c32e7e3b684dcca6b9f0f2718
Octopus <klinov.artem@gmail.com>
2024-02-19 20:19:38 +0600

    bot_movie com63 state main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from loader import bot
+"""Запуск бота."""
+
 import handlers
+
+from loader import bot
+from telebot.custom_filters import StateFilter
 from utils.set_bot_commands import set_default_commands
-# from utils.set_bot_commands import set_custom_commands
 
 
 if __name__ == '__main__':
+    bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
-    # set_custom_commands(bot)
     bot.infinity_polling()
 
  
 # TODO 
     # help доработать...
+    # user states
