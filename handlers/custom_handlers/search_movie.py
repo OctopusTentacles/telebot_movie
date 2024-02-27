4a037f6ced9ea940a63837aa85040362785a1e20
Octopus <klinov.artem@gmail.com>
2024-02-27 20:52:49 +0600

    bot_movie com96 user_input_title

--- handlers/custom_handlers/search_movie.py
+++ handlers/custom_handlers/search_movie.py
@@ -1,1 +1,21 @@
 """Модуль команды movie из кнопки ГЛАВНОГО МЕНЮ - Поиск фильма."""
+
+
+
+
+from loader import bot
+from log_data import logger
+from states.user_input import UserInputState
+from telebot.types import CallbackQuery
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'movie')
+def user_input_title(call: CallbackQuery):
+    bot.set_state(call.message.chat.id, state = UserInputState.search_movie)
+
+    current_state = bot.get_state(call.message.chat.id)
+    logger.info(f'Current state: {current_state}')
+
+    bot.send_message(call.message.chat.id, 'Введи название фильма:')
+    bot.register_next_step_handler(call.message, create_url, bot)
+
