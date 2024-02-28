61177f75f1a49f8a1760f7aba7e20f436a8688f7
Octopus <klinov.artem@gmail.com>
2024-02-28 13:47:24 +0600

    bot_movie com100 search_movie_api/url

--- api/search_movie_api.py
+++ api/search_movie_api.py
@@ -12,5 +12,22 @@ from log_data import logger
 
 
 @logger.catch
-def search_movie_api():
-    ...
+def search_movie_api(encoding_title):
+    url = (
+        'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=1&'
+        f'query={encoding_title}'
+    )
+
+    headers = {'accept': 'application/json', "X-API-KEY": config.API_KEY}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        contents = data.get('docs')
+        # получили краткую информацию о фильме,
+        # но мы хотим полную информацию - поэтому,
+        # берем ID, формируем ссылку по ID - это даст полную информацию.
+        for content in contents:
+            id = content.get('id')
+            
+        url = f'https://api.kinopoisk.dev/v1.4/movie/{id}'
