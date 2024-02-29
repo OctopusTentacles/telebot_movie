2b0fce543c1c4050036d9fd58c21614b9d212fb3
Octopus <klinov.artem@gmail.com>
2024-02-29 11:03:12 +0600

    bot_movie com106 search_handler/user_input_name

--- handlers/custom_handlers/search_handler.py
+++ handlers/custom_handlers/search_handler.py
@@ -22,15 +22,22 @@ def user_input_title(call: CallbackQuery):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'person')
 def user_input_name(call: CallbackQuery):
-    bot.set_state()
+    bot.set_state(call.message.chat.id, state = UserInputState.search_name)
+
+    current_state = bot.get_state(call.message.chat.id)
+    logger.info(f'Current state: {current_state}')
+
+    bot.send_message(call.message.chat.id, 'Введи имя:')
+
+
 
 # Получить введенное пользователем название и закодировать:
 def encoding_title(message):
-    user_title = message.text.strip()
-    print(user_title)
+    user_input = message.text.strip()
+    print(user_input)
 
-    encoding_title = quote(user_title)
-    print(encoding_title)
+    encoding_input = quote(user_input)
+    print(encoding_input)
 
-    url = create_url_api.create_url_api(encoding_title)
+    url = create_url_api.create_url_api(encoding_input)
     print(url)
