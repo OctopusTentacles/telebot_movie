52aece2f19ceb4b1a761f595a5e3b53d707564b2
Octopus <klinov.artem@gmail.com>
2024-03-04 12:30:20 +0600

    bot_movie com116 callback_search_movie done

--- handlers/custom_handlers/search_handler.py
+++ handlers/custom_handlers/search_handler.py
@@ -1,4 +1,5 @@
-"""Модуль команды movie из кнопки ГЛАВНОГО МЕНЮ - Поиск фильма."""
+"""Модуль поиска кнопки ГЛАВНОГО МЕНЮ - Поиск фильма и Поиск Актера.
+"""
 
 
 import requests
@@ -41,61 +42,36 @@ def callback_search_movie(message):
 
     text_1, text_2, poster = search_movie_api.search_movie_api(url)
 
-    print("Text 1:", text_1)
-    print("Text 2:", text_2)
-    print("Poster URL:", poster)
+    response = requests.post(
+        f"https://api.telegram.org/bot{bot.token}/sendPhoto",
+        data={
+            "chat_id": message.chat.id,
+            "photo": poster,
+            "caption": text_1
+        }
+    )
+    # Обработка ответа от сервера Telegram
+    if response.status_code != 200:
+        logger.error(
+            f"Failed to send photo: {response.status_code}, {response.text}"
+        )
+    # отправляем описание,
+    # может превышать макс размер 4096.
+    bot.send_message(message.chat.id, text_2[:4096])
+    if len(text_2) > 4096:
+        bot.send_message(message.chat.id, text_2[4096:])
 
 
 
 
 
-# @bot.callback_query_handler(func=lambda call: call.data == 'person')
-# def user_input_name(call: CallbackQuery):
-#     bot.set_state(call.message.chat.id, state = UserInputState.search_name)
 
-#     current_state = bot.get_state(call.message.chat.id)
-#     logger.info(f'Current state: {current_state}')
+@bot.callback_query_handler(func=lambda call: call.data == 'person')
+def user_input_name(call: CallbackQuery):
+    bot.set_state(call.message.chat.id, state = UserInputState.search_name)
 
-#     bot.send_message(call.message.chat.id, 'Введи имя:')
+    current_state = bot.get_state(call.message.chat.id)
+    logger.info(f'Current state: {current_state}')
 
-
-
-    
-
-
-
-# @bot.callback_query_handler(
-#     func=lambda call: call.data == 'movie'
-# )
-# def callback_search_movie(call: CallbackQuery):
-#     global url
-
-#     print(call.data)
-#     print('callback_search_movie', url)
-#     print(bot.get_state(call.message.chat.id))
-
-#     text_1, text_2, poster = search_movie_api.search_movie_api(url)
-
-#     print("Text 1:", text_1)
-#     print("Text 2:", text_2)
-#     print("Poster URL:", poster)
-
-#     response = requests.post(
-#         f"https://api.telegram.org/bot{bot.token}/sendPhoto",
-#         data={
-#             "chat_id": call.message.chat.id,
-#             "photo": poster,
-#             "caption": text_1
-#         }
-#     )
-#     # Обработка ответа от сервера Telegram
-#     if response.status_code != 200:
-#         logger.error(
-#             f"Failed to send photo: {response.status_code}, {response.text}"
-#         )
-#     # отправляем описание,
-#     # может превышать макс размер 4096.
-#     bot.send_message(call.message.chat.id, text_2[:4096])
-#     if len(text_2) > 4096:
-#         bot.send_message(call.message.chat.id, text_2[4096:])
+    bot.send_message(call.message.chat.id, 'Введи имя:')
 
