7f9343bf8b4500cf162f8a1d6168f669ca89ce36
Octopus <klinov.artem@gmail.com>
2024-04-12 14:39:49 +0500

    bot_movie com133 db_manager

--- database/db_manager.py
+++ database/db_manager.py
@@ -7,9 +7,20 @@ from peewee import IntegrityError
 
 def save_user_registration(user_id, user_name):
     try:
-        UserRequest.create(
+        existing_user = UserRequest.select().where(
+            UserRequest.user_id == user_id
+        ).first()
+        if existing_user:
+            return True
+
+        new_user = UserRequest.create(
             user_id = user_id,
             user_name = user_name,
             category = 'registration'
         )
-    except IntegrityError:
+        return True
+    
+
+    except Exception:
+        return False
+    
