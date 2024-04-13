e573b858c93f750add0ed01e7139e984eb295603
Octopus <klinov.artem@gmail.com>
2024-04-13 11:32:55 +0500

    bot_movie com139 db_manager registration

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,26 +1,42 @@
+""" Модуль проверки регистрации пользователей. """
 
 
-
-from models import UserRequest
+from datetime import datetime
+from .models import UserRegistration
 from peewee import IntegrityError
 
 
-def save_user_registration(user_id, user_name):
+def save_user_registration(user_id: int, user_name: str) -> bool:
+    """Сохраняет регистрацию пользователя в базе данных.
+
+    Args:
+        user_id (int): Идентификатор пользователя.
+        user_name (str): Имя пользователя.
+
+    Returns:
+        bool: True, если регистрация прошла успешно,
+              False в противном случае.
+    """
     try:
-        existing_user = UserRequest.select().where(
-            UserRequest.user_id == user_id
-        ).first()
+        existing_user = UserRegistration.select().where(
+            UserRegistration.user_id == user_id).first()
+        
         if existing_user:
             return True
 
-        new_user = UserRequest.create(
+        new_user = UserRegistration.create(
             user_id = user_id,
             user_name = user_name,
-            category = 'registration'
+            favorite_movies = '',
+            registration_date = datetime.now()
         )
         return True
     
 
-    except Exception:
+    except IntegrityError:
+        # Если пользователь с таким user_id уже существует:
         return False
-    
+    except Exception as exc:
+        # Обработка других исключений:
+        print('Ошибка в базе данных', exc)
+        return False    
