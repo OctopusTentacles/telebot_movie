d414c0e0bb1c0ca61e348cbcf8e25b5bd51bbcb2
Octopus <klinov.artem@gmail.com>
2024-04-13 13:06:51 +0500

    bot_movie com146 regs_handler need new state

--- handlers/custom_handlers/regs_handler.py
+++ handlers/custom_handlers/regs_handler.py
@@ -2,7 +2,7 @@
 
 
 from database import save_user_registration
-from loader import TeleBot
+from loader import bot
 from log_data import logger
 from states.user_input import UserInputState
 from telebot.types import CallbackQuery
@@ -10,6 +10,8 @@ from telebot.types import CallbackQuery
 
 @bot.callback_query_handler(func=lambda call: call.data == 'register')
 def register_user(call: CallbackQuery):
+    bot.set_state(call.message.chat.id, UserInputState.register)
+
     user_id = call.from_user.id
     user_name = call.from_user.full_name
     if save_user_registration(user_id, user_name):
