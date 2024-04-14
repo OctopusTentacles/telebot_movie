9c5eba258a0b9b53f35ec502903710df47a45fb2
Octopus <klinov.artem@gmail.com>
2024-04-14 19:06:39 +0500

    bot_movie com150 register_user current_state

--- handlers/custom_handlers/regs_handler.py
+++ handlers/custom_handlers/regs_handler.py
@@ -11,10 +11,14 @@ from telebot.types import CallbackQuery
 @bot.callback_query_handler(func=lambda call: call.data == 'register')
 def register_user(call: CallbackQuery):
     bot.set_state(call.message.chat.id, UserInputState.register)
+    
+    current_state = bot.get_state(call.message.chat.id)
+    logger.info(f'Current state: {current_state}')
 
     user_id = call.from_user.id
     user_name = call.from_user.full_name
     if save_user_registration(user_id, user_name):
+        logger.info()
         bot.send_message(
             call.message.chat.id,
             'Вы успешно зарегистрированы!'
