10db302705e00f701993b3fda48d76fc3e022ec2
Octopus <klinov.artem@gmail.com>
2024-04-15 16:12:57 +0500

    bot_movie com160 regs_handler docs

--- handlers/custom_handlers/regs_handler.py
+++ handlers/custom_handlers/regs_handler.py
@@ -1,7 +1,13 @@
-"""Модуль кнопки ЗАРЕГИСТРИРОВАТЬСЯ."""
+"""
+Модуль кнопки ЗАРЕГИСТРИРОВАТЬСЯ.
+
+Этот модуль содержит обработчик кнопки регистрации пользователя.
+"""
 
 
 from database import save_user_registration
+from default_handlers.start import bot_start
+
 from loader import bot
 from log_data import logger
 from states.user_input import UserInputState
@@ -10,6 +16,21 @@ from telebot.types import CallbackQuery
 
 @bot.callback_query_handler(func=lambda call: call.data == 'register')
 def register_user(call: CallbackQuery):
+    """
+    Обработчик нажатия кнопки регистрации.
+
+    Эта функция вызывается при нажатии пользователем кнопки "Зарегистрироваться".
+    Она устанавливает состояние пользователя в режим регистрации,
+    сохраняет информацию о пользователе в базе данных при успешной регистрации,
+    и вызывает функцию bot_start для продолжения работы с пользователем.
+
+    Args:
+        call (CallbackQuery): Объект, представляющий вызов 
+                                обратного запроса от пользователя.
+
+    Returns:
+        None
+    """
     bot.set_state(call.message.chat.id, UserInputState.register)
 
     current_state = bot.get_state(call.message.chat.id)
@@ -22,9 +43,12 @@ def register_user(call: CallbackQuery):
             f'User {user_name} with ID {user_id} successfully registered.'
         )
         bot.send_message(
-            call.message.chat.id,
-            'Вы успешно зарегистрированы!'
+            call.message.chat.id, 'Вы успешно зарегистрированы!'
         )
+        
+        # После успешной регистрации вызываем функцию bot_start:
+        bot_start(call.message)
+        
     else:
         bot.send_message(
             call.message.chat.id,
