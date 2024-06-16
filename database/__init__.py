9497af51c0d3e41758815f4c16cc75a02a9e2e83
Octopus <klinov.artem@gmail.com>
2024-06-16 21:13:48 +0500

    bot_movie com169 inits for favorite

--- database/__init__.py
+++ database/__init__.py
@@ -2,3 +2,4 @@ from .models import UserRequest, UserRegistration
 from .db_manager import check_user_registration
 from .db_manager import save_user_registration
 from .db_manager import get_user_name
+from .db_manager import add_favorite_movie
--- handlers/default_handlers/__init__.py
+++ handlers/default_handlers/__init__.py
@@ -1,3 +1,4 @@
 from . import start
 from . import main_menu
 from . import help
+from . import favorite
