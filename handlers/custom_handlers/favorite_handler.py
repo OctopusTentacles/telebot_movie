9f7a52219e6be3f9621ea2492298ffb4c07c9b90
Octopus <klinov.artem@gmail.com>
2024-06-17 21:43:47 +0500

    bot_movie com177 favorite_handler

---
+++ handlers/custom_handlers/favorite_handler.py
@@ -0,0 +1,42 @@
+"""Модуль команды favorite из кнопки ГЛАВНОГО МЕНЮ Избранное."""
+
+
+from keyboards.inline import type_keyboard
+from loader import bot
+from log_data import logger
+from states.user_input import UserInputState
+from telebot.types import CallbackQuery
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'favorite')
+def show_favorite_movies(call: CallbackQuery):
+    """
+    Обработчик нажатия кнопки "Избранное".
+
+    Args:
+        call (CallbackQuery): Объект, 
+        представляющий вызов обратного запроса от пользователя.
+
+    Returns:
+        None
+    """
+    bot.set_state(call.message.chat.id, state = UserInputState.favorite)
+
+    current_state = bot.get_state(call.message.chat.id)
+    logger.info(f'Current state: {current_state}')
+
+    user_id = call.from_user.id
+    # Получение списка избранных фильмов
+    favorite_movies = get_favorite_movies(user_id)
+
+    if favorite_movies:
+        movie_list = '\n'.join(f'{movie}'for movie in favorite_movies)
+        bot.send_message(
+            call.message.chat.id,
+            f'Ваши избранные фильмы:\n{movie_list}'
+        )    
+    else:
+        bot.send_message(
+            call.message.chat.id,
+            'Ваш список избранных фильмов пуст.'
+        )
