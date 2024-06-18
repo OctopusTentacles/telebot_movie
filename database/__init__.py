90055e9440e6c437d6c9091d651a07fd0d363654
Octopus <klinov.artem@gmail.com>
2024-06-18 22:36:03 +0500

    bot_movie com179 init get_favorite_movie

--- database/__init__.py
+++ database/__init__.py
@@ -3,3 +3,4 @@ from .db_manager import check_user_registration
 from .db_manager import save_user_registration
 from .db_manager import get_user_name
 from .db_manager import add_favorite_movie
+from .db_manager import get_favorite_movie
