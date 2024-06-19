553c5b64b607d6cc60f20fc2a6fb2494897de28c
Octopus <klinov.artem@gmail.com>
2024-06-19 22:24:00 +0500

    bot_movie com185 history.py

--- handlers/default_handlers/history.py
+++ handlers/default_handlers/history.py
@@ -10,8 +10,24 @@ from telebot.types import Message
 @bot.message_handler(commands=['history'])
 @logger.catch
 def user_history(message: Message):
-    """_summary_
+    """
+    Обработчик команды /history.
+
+    Эта функция вызывается при вводе пользователем команды /history.
+    Она получает историю пользователя из базы данных 
+    и отправляет её в чат.
 
     Args:
-        message (Message): _description_
+        message (Message): Сообщение с командой от пользователя.
+
+    Returns:
+        None
     """
+    bot.set_state(message.chat.id, UserInputState.history)
+    current_state = bot.get_state(message.chat.id)
+    logger.info(f"Current state: {current_state}")
+
+
+    user_id = message.from_user.id
+    history = get_user_history(user_id)
+    bot.send_message(message.chat.id, history)
